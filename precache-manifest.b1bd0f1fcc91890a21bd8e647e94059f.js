52ea83d18ad96f9978d3bd163ce12f27be14592b
Peeranut Pongpakatien <peeranut32@gmail.com>
2021-01-06 18:38:04 +0700

    deploy

---
+++ precache-manifest.b1bd0f1fcc91890a21bd8e647e94059f.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8dbf86154064b2ea473",
+    "url": "/gogo-landing-page/css/app.673fc470.css"
+  },
+  {
+    "revision": "e8dbf86154064b2ea473",
+    "url": "/gogo-landing-page/js/app.d660f970.js"
+  },
+  {
+    "revision": "280f444ee405a23ea5b5",
+    "url": "/gogo-landing-page/css/chunk-4e0b8328.1fead564.css"
+  },
+  {
+    "revision": "280f444ee405a23ea5b5",
+    "url": "/gogo-landing-page/js/chunk-4e0b8328.af5c9b88.js"
+  },
+  {
+    "revision": "accb3967cdd996d9d63d",
+    "url": "/gogo-landing-page/js/chunk-52e4de86.9dd99650.js"
+  },
+  {
+    "revision": "fc38122e1fee4f067a12",
+    "url": "/gogo-landing-page/js/chunk-vendors.40bde69a.js"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/gogo-landing-page/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/gogo-landing-page/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/gogo-landing-page/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/gogo-landing-page/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/gogo-landing-page/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/gogo-landing-page/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/gogo-landing-page/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/gogo-landing-page/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/gogo-landing-page/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/gogo-landing-page/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "16fd1e3940ebc70c32ba8b1c4b5d12e2",
+    "url": "/gogo-landing-page/index.html"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/gogo-landing-page/favicon.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/gogo-landing-page/img/brand/blue.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/gogo-landing-page/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/gogo-landing-page/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/gogo-landing-page/img/brand/favicon.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/gogo-landing-page/img/brand/white.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/gogo-landing-page/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/gogo-landing-page/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/gogo-landing-page/img/theme/landing.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/gogo-landing-page/img/theme/promo-1.png"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/gogo-landing-page/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/gogo-landing-page/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/gogo-landing-page/img/theme/profile.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/gogo-landing-page/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/gogo-landing-page/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/gogo-landing-page/img/theme/team-4-800x800.jpg"
+  }
+];
