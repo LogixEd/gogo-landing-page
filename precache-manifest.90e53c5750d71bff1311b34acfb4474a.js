ad2c5d81a47265565b899fd6e250ab13d5303b6e
Toro <kamper543@gmail.com>
2022-01-09 20:40:52 +0700

    deploy

---
+++ precache-manifest.90e53c5750d71bff1311b34acfb4474a.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = [
+  {
+    "revision": "0df4d1488cf7c4804c27",
+    "url": "/css/app.8e3b3ecc.css"
+  },
+  {
+    "revision": "0df4d1488cf7c4804c27",
+    "url": "/js/app.9714267c.js"
+  },
+  {
+    "revision": "5a390e68203a495ec626",
+    "url": "/js/chunk-vendors.202855b5.js"
+  },
+  {
+    "revision": "e5134c06fc1cdb6004b14abc231ffe3d",
+    "url": "/img/seeed-logo.e5134c06.png"
+  },
+  {
+    "revision": "d1fa5e1e15bff1549802d47e260ba88f",
+    "url": "/img/project3.d1fa5e1e.gif"
+  },
+  {
+    "revision": "3d612f98f638572225a5a293698e3ee0",
+    "url": "/img/arduino-logo.3d612f98.png"
+  },
+  {
+    "revision": "a3abfe0f4448f55d61dd57eb328ee9d9",
+    "url": "/img/project5.a3abfe0f.gif"
+  },
+  {
+    "revision": "2d85643bd8d75cabeb5f82c0a465bf77",
+    "url": "/img/Customers.2d85643b.png"
+  },
+  {
+    "revision": "c434d85727cf8683922d65e00589f948",
+    "url": "/img/logo-lil.c434d857.png"
+  },
+  {
+    "revision": "d14312aa47530b3b076de19ff258ced2",
+    "url": "/img/GoGo6-1.d14312aa.jpg"
+  },
+  {
+    "revision": "aad5912c08129a94076bc240f7de4f83",
+    "url": "/img/scratch-with-cat.aad5912c.jpeg"
+  },
+  {
+    "revision": "db5d4779165ba471996d3bede4280ef8",
+    "url": "/img/project7.db5d4779.gif"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "4cbf9bfe070c6490fe262a01ec1fdf9c",
+    "url": "/img/gogopic.4cbf9bfe.png"
+  },
+  {
+    "revision": "9be0aa6bc964bd7b03cb33dbe29799bc",
+    "url": "/img/Partners.9be0aa6b.png"
+  },
+  {
+    "revision": "494cb5a2f0c105a4fccf77c068578f52",
+    "url": "/img/facebook1.494cb5a2.png"
+  },
+  {
+    "revision": "3b703239c7604690ec3c8477ed38dd51",
+    "url": "/img/gogo-logo.3b703239.png"
+  },
+  {
+    "revision": "13e956d3ecbecb8c44bc3bbe3e58776b",
+    "url": "/img/line1.13e956d3.png"
+  },
+  {
+    "revision": "e0d9635ccc34cb0af5d538cd5a131f4c",
+    "url": "/img/GoGo5-1.e0d9635c.jpg"
+  },
+  {
+    "revision": "4f7cb5fbb28d0f9ddc97641f8b288735",
+    "url": "/img/instagram1.4f7cb5fb.png"
+  },
+  {
+    "revision": "445d559954dc07a952aaa30138485e5b",
+    "url": "/img/project2.445d5599.gif"
+  },
+  {
+    "revision": "c2c1fa1cfff3efb7dabdc8b7d42b0699",
+    "url": "/img/logo-gogo.c2c1fa1c.png"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b303f620f207c68e63e4e42dec2e66da",
+    "url": "/img/project1.b303f620.gif"
+  },
+  {
+    "revision": "0b0d8da5861f0e1797b6bd09f53b004b",
+    "url": "/img/GoGoB.0b0d8da5.jpg"
+  },
+  {
+    "revision": "1df13749fef73a461ca6b88baadfcffb",
+    "url": "/img/project6.1df13749.gif"
+  },
+  {
+    "revision": "ea74d665debcbd360598c3e3d64e5214",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7afc9cbf3456784c68697002ff0acd5e",
+    "url": "/favicon.icns"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "9f016db9c33b75dc050fdba9703ba42d",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  }
+];
